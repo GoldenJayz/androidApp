929f41760a5aed7b854b183bdcb3cf7d95aad698
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2023-10-21 17:06:08 -0400

    Added Card

--- src/App.tsx
+++ src/App.tsx
@@ -15,22 +15,41 @@ import 'react-native';
 import {View, Text, StyleSheet} from 'react-native';
 import MusicPlayer from './Components/MusicPlayer/MusicPlayer';
 import { SetupService } from './services/SetupService';
+import Card from './Components/MusicPlayer/Card';
 
+// UseEffect for when the app loads it grabs all the songs and asynchronously adds all the songs. Fetches from API to see what songs are availible
+// Use the components state in order to store the song data
 
 const App: () => JSX.Element = () => {
 
   React.useEffect(() => {
+    // Cant manipulate the state from the parent
+    // Just iterate through a list that you fetch from the api then pass the data as a prop
+    fetch('http://192.168.1.17:3000/songs')
+    .then(res => res.json())
+    .then((body) => {
+      for (let i = 0; i < body.length; i++) {
+        let song = body[i]
+        console.log(song);
+      }
+    });
     try {
       SetupService();
     }
-    catch (e) {}
+    catch (e) {
+
+    }
 
   }, []);
 
   return (
     <View style={styles.body}>
-      <Text>Testing</Text>
-      <MusicPlayer />
+
+      <View style={styles.container}>
+        <Card song='bob' />
+        <Card song='dylan' />
+      </View>
+
     </View>
   );
 };
@@ -39,6 +58,14 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: '#121212',
     height: '100%',
+  },
+
+  container: {
+    flexDirection: 'row'
+  },
+
+  row: {
+    flexDirection: 'row'
   }
 });
 
--- src/Components/MusicPlayer/Card.tsx
+++ src/Components/MusicPlayer/Card.tsx
@@ -1,18 +1,47 @@
 import * as React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ImageBackground, Image, TouchableWithoutFeedback} from 'react-native';
+import MusicPlayer from './MusicPlayer';
+import * as songData from '../../Data/playlist.json';
 
 interface CardProps {
-
+  song: string
 }
 
-const Card: React.FC<CardProps> = ({}: CardProps) => {
+const Card: React.FC<CardProps> = ({song}: CardProps) => {
   return (
-    <View>
-
-    </View>
+    <TouchableWithoutFeedback onPress={() => {
+      /* 
+        Depending on the card being clicked a different song will play
+      */
+      console.log('fart')
+    }}>
+      <View>
+        <ImageBackground style={styles.ImageStyles} source={require('../../Data/Images/box.png')}>
+          <Image style={styles.image} source={{
+            uri: 'https://rntp.dev/example/Longing.jpeg'
+          }} />
+          <Text>{ song }</Text>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  ImageStyles: {
+    height: 150,
+    width: 150,
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100
+  },
+
+  image: {
+    position: 'relative',
+    width: 100,
+    height: 100
+  }
+});
 
 export default Card;
